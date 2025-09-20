8e0b52aa7284355a56ed2097978b0d771255e8f2
thanhphongdang <thanhphong25@gmail.com>
2025-09-20 17:55:52 +0700

    Vagrant + Docker

---
+++ sources/training-php/training-php/configs/database.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST', 'localhost');
+// define('DB_HOST', 'web-mysql');
+define('DB_USER', 'user');
+define('DB_PASSWORD', 'pass');
+define('DB_PORT', 3306);
+define('DB_NAME', 'app_web1');
---
+++ sources/training-php/training-php/models/BaseModel.php
@@ -0,0 +1,82 @@
+<?php
+require_once 'configs/database.php';
+
+abstract class BaseModel {
+    // Database connection
+    protected static $_connection;
+
+    public function __construct() {
+
+        // echo "";
+        // echo "</br>";
+        // echo DB_HOST;
+        // echo "</br>";
+        // echo DB_USER;
+        // echo "</br>";
+        // echo DB_PASSWORD;
+        // echo "</br>";
+        // exit();
+        if (!isset(self::$_connection)) {
+            self::$_connection = mysqli_connect('web-mysql', DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
+            if (self::$_connection->connect_errno) {
+                printf("Connect failed");
+                exit();
+            }
+        }
+
+    }
+
+    /**
+     * Query in database
+     * @param $sql
+     */
+    protected function query($sql) {
+        $result = self::$_connection->query($sql);
+        return $result;
+    }
+
+    /**
+     * Select statement
+     * @param $sql
+     */
+    protected function select($sql) {
+        $result = $this->query($sql);
+        $rows = [];
+        if (!empty($result)) {
+            while ($row = $result->fetch_assoc()) {
+                $rows[] = $row;
+            }
+        }
+        return $rows;
+    }
+
+    /**
+     * Delete statement
+     * @param $sql
+     * @return mixed
+     */
+    protected function delete($sql) {
+        $result = $this->query($sql);
+        return $result;
+    }
+
+    /**
+     * Update statement
+     * @param $sql
+     * @return mixed
+     */
+    protected function update($sql) {
+        $result = $this->query($sql);
+        return $result;
+    }
+
+    /**
+     * Insert statement
+     * @param $sql
+     */
+    protected function insert($sql) {
+        $result = $this->query($sql);
+        return $result;
+    }
+
+}
